214d54affa067c00fb63b89350d24efbcedd4571
1346 <961131804@qq.com>
2018-01-14 18:37:30 +0800

    next

--- src/index.vue
+++ src/index.vue
@@ -8,10 +8,6 @@
   export default {
     data() {
       return {
-        ceshi: '这是一个测试数据',
-        ceshiAgain: '这是一个又是测试数据',
-        ceshixixixi: 'asfasfasfa',
-        addOther: '修改master分支上的同一文件同一地方',
         next: '新建next分支'
       }
     },

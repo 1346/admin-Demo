c2d7e6546cc3ed8a30ffe047c49faff42006dd9b
1346 <961131804@qq.com>
2018-04-10 14:10:22 +0800

    add dist

---
+++ dist/static/js/11.f4f384ad2f84d0df53da.js
@@ -0,0 +1,2 @@
+webpackJsonp([11],{"+abo":function(e,n,o){"use strict";Object.defineProperty(n,"__esModule",{value:!0});var l={name:"authredirect",created:function(){var e=window.location.search.slice(1);window.opener.location.href=window.location.origin+"/login#"+e,window.close()}},i=o("VU/8")(l,null,!1,null,null,null);n.default=i.exports}});
+//# sourceMappingURL=11.f4f384ad2f84d0df53da.js.map
